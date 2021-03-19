0ecb01f322d419098f4877550eb33291cd72a902
ricardo8a <ricardo.diaz.8a@gmail.com>
2021-03-18 23:05:18 -0600

    add Header, Content and tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable global-require */
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+};
+/* eslint-enable global-require */
